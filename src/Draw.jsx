38da101178bed85fecdb74328d12a03bad49bc31
ArnavMeh <mehraarn000@gmail.com>
2024-05-12 12:51:15 -0400

    ok

--- src/Draw.jsx
+++ src/Draw.jsx
@@ -1,5 +1,29 @@
-const Draw = () => {
+import { useEffect, useRef } from "react";
 
+const Draw = ({
+  drawInstr,
+  str
+}) => {
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    /** @type { CanvasRenderingContext2D } */
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log({ctx});
+
+    // for (const ch of str) {
+    //   drawInstr.
+    // }
+    // ctx.lineTo()
+  }, [drawInstr, str])
+
+  return (
+    <canvas
+      ref={canvasRef}
+    />
+  )
 };
 
 export default Draw;
